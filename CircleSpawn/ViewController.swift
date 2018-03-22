5f5baaf8194b0cc00adef696f133e915830b3d3f
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-03-22 03:27:53 +0100

    pan and longpress gesture added

--- CircleSpawn/ViewController.swift
+++ CircleSpawn/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var circleView: UIView!
     
     @IBOutlet weak var viewTap: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        circleView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
-        circleView.layer.cornerRadius = 50
-        circleView.backgroundColor = UIColor.randomBrightColor()
-        
         
         tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.myViewTapped))
         tapGesture.numberOfTapsRequired = 2
@@ -27,19 +22,66 @@ class ViewController: UIViewController {
         viewTap.addGestureRecognizer(tapGesture)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
     
     var tapGesture  = UITapGestureRecognizer()
+    
     @objc func myViewTapped(touch: UITapGestureRecognizer) {
         let touchPoint = touch.location(in: self.view)
-        let circleView1 = UIView(frame: CGRect(x: touchPoint.x, y: touchPoint.y, width: 100, height: 100))
-        circleView1.backgroundColor = UIColor.randomBrightColor()
-        circleView1.layer.cornerRadius = 50
-        self.view.addSubview(circleView1)
+        let circleView = UIView(frame: CGRect(x: touchPoint.x - 50, y: touchPoint.y - 50, width: 100, height: 100))
+        circleView.backgroundColor = UIColor.randomBrightColor()
+        circleView.layer.cornerRadius = 50
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        longPressRecognizer.minimumPressDuration = 1
+        longPressRecognizer.delegate = self
+        circleView.addGestureRecognizer(longPressRecognizer)
+        self.view.addSubview(circleView)
+    }
+    
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer)  {
+        print("long press detected")
+        if recognizer.state == UIGestureRecognizerState.began {
+            let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+            panRecognizer.delegate = self
+            recognizer.view?.addGestureRecognizer(panRecognizer)
+            if let view = recognizer.view {
+                UIView.animate(withDuration: 0.5, animations: {
+                    view.frame.size.width += 10
+                    view.frame.size.height += 10
+                    view.layer.cornerRadius += 5
+                    view.alpha = 0.5
+                }, completion: nil)
+            }
+            
+            print("long press began")
+        }
+        if recognizer.state == UIGestureRecognizerState.ended{
+            if let view = recognizer.view {
+                UIView.animate(withDuration: 0.5, animations: {
+                    view.frame.size.width -= 10
+                    view.frame.size.height -= 10
+                    view.layer.cornerRadius -= 5
+                    view.alpha = 1
+                }, completion: nil)
+            }
+        }
+    }
+    @objc func handlePan(recognizer: UIPanGestureRecognizer)  {
+        print("pan detected")
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
         
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+        
+    }
+    
+    
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
 
@@ -52,6 +94,8 @@ extension CGFloat {
         assert(max > min)
         return min + ((max - min) * CGFloat(arc4random()) / CGFloat(UInt32.max))
     }
+    
+    
 }
 
 extension UIColor {
@@ -61,4 +105,7 @@ extension UIColor {
                        brightness: .random(min: 0.7, max: 1.0),
                        alpha: 1.0)
     }
+}
+extension ViewController: UIGestureRecognizerDelegate {
+    
 }
