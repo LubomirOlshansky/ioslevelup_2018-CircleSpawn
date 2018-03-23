97f636d5fc4ad0aa111ce3107d1e94c26809218a
Lubomir Olshansky <ol.lub.ua@gmail.com>
2018-03-23 03:02:47 +0100

    Drug and drop fixed. Triple tap for deleting added

--- CircleSpawn/ViewController.swift
+++ CircleSpawn/ViewController.swift
@@ -10,66 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var initialCenter: CGPoint?
+    var initialPoint: CGPoint?
+    var correct: CGPoint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.myViewTapped))
+        doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.myViewDoubleTapped))
         doubleTapGesture.numberOfTapsRequired = 2
-        doubleTapGesture.numberOfTouchesRequired = 1
         doubleTapGesture.delegate = self
-        tripleTapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.myViewTapped))
-        tripleTapGesture.numberOfTapsRequired = 3
-        tripleTapGesture.numberOfTouchesRequired = 1
-        tripleTapGesture.delegate = self
         self.view.addGestureRecognizer(doubleTapGesture)
     }
     
     var doubleTapGesture  = UITapGestureRecognizer()
     var tripleTapGesture  = UITapGestureRecognizer()
     
-    @objc func myViewTapped(touch: UITapGestureRecognizer) {
+    @objc func myViewDoubleTapped(touch: UITapGestureRecognizer) {
         let touchPoint = touch.location(in: self.view)
         let circleView = UIView(frame: CGRect(x: touchPoint.x - 50, y: touchPoint.y - 50, width: 100, height: 100))
         circleView.backgroundColor = UIColor.randomBrightColor()
         circleView.layer.cornerRadius = 50
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
-        longPressRecognizer.minimumPressDuration = 0.3
+        longPressRecognizer.minimumPressDuration = 0.15
         longPressRecognizer.delegate = self
         circleView.addGestureRecognizer(longPressRecognizer)
+        tripleTapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.myViewTripleTapped))
+        tripleTapGesture.numberOfTapsRequired = 3
+        tripleTapGesture.delegate = self
+        doubleTapGesture.require(toFail: tripleTapGesture)
         circleView.addGestureRecognizer(tripleTapGesture)
         self.view.addSubview(circleView)
+    }
+    
+    @objc func myViewTripleTapped(touch: UITapGestureRecognizer) {
+        guard let view = touch.view else {
+            return
+        }
+       view.removeFromSuperview()
     }
     
     @objc func handleLongPress(recognizer: UILongPressGestureRecognizer)  {
         print("long press detected")
         if recognizer.state == UIGestureRecognizerState.began {
              print("long press began")
-            if let view = recognizer.view {
-                UIView.animate(withDuration: 0.5, animations: {
-                    view.frame.size.width += 10
-                    view.frame.size.height += 10
-                    view.layer.cornerRadius += 5
-                    view.alpha = 0.5
-                }, completion: nil)
-            }
-        }
-        else if recognizer.state == .changed {
-             print("long press changed")
             guard let view = recognizer.view else {
                 return
             }
-
+                UIView.animate(withDuration: 0.3, animations: {
+                    view.frame.size.width += 20
+                    view.frame.size.height += 20
+                    view.layer.cornerRadius += 10
+                    view.alpha = 0.5
+                }, completion: nil)
+            
+            initialCenter = view.center
+            initialPoint = recognizer.location(in: self.view)
+            guard let originalCenter = initialCenter else {
+                return
+            }
+            guard let originalPoint = initialPoint else {
+                return
+            }
+            correct = CGPoint(x:originalPoint.x - originalCenter.x, y:originalPoint.y - originalCenter.y)
+    
+        }
+        else if recognizer.state == .changed {
+             print("long press changed")
+            guard let correction = correct else {
+                return
+            }
+            guard let view = recognizer.view else {
+                return
+            }
+        
             let location = recognizer.location(in: self.view)
-            view.center = CGPoint(x:view.center.x + (location.x - view.center.x),
-                                              y:view.center.y + (location.y - view.center.y))
+            view.center = CGPoint(x:(view.center.x - correction.x) + (location.x - view.center.x),
+                                              y:(view.center.y - correction.y) + (location.y - view.center.y))
         }
         else if recognizer.state == UIGestureRecognizerState.ended{
              print("long press ended")
             if let view = recognizer.view {
                 UIView.animate(withDuration: 0.5, animations: {
-                    view.frame.size.width -= 10
-                    view.frame.size.height -= 10
-                    view.layer.cornerRadius -= 5
+                    view.frame.size.width -= 20
+                    view.frame.size.height -= 20
+                    view.layer.cornerRadius -= 10
                     view.alpha = 1
                 }, completion: nil)
             }
